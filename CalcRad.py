eb77c476c85ffdf182a6436869adc31d036e851e
Nasrah963 <nasrahussein422@gmail.com>
2022-01-11 22:43:36 +0200

    edit3

--- CalcRad.py
+++ CalcRad.py
@@ -6,12 +6,17 @@ gd = graph(width=600, height=250,
     xtitle='<i>Lamda</i>', ytitle='<i>Radiation</i>',
     foreground=color.black, background=color.white)
 curve= gcurve(color=color.black)
-# h=6.626*10**-34
-# c=3*10**8
-# k=1.38*(10**-23)
-for y in arange(2,14,2):
-    B=y**2
-    # B=(2*float(h)*(float(c)**2) / (float(y)**5)) / ( exp**( (float(h)*float(c))/(float(y)*float(k)*float(T)) ) -1 )
+h=6.626*10**-34
+c=3*10**8
+k=1.38*(10**-23)
+T=5777
+for y in arange(2,40,2)*10**-9:
+    # B=y**2
+    D=(2*h* (c**2) )/(y)**5
+    F=(h*c)/(y*k*T)
+    G=(2.718**(F))
+    B=D/(G-1)
+    
     curve.plot(y,B)
 
 # def max_lamda(T):
